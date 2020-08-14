1df55751b02d3ff893431e817a65d3acd12eed11
Artur Júnior <artur.junior@soluti.com.br>
2020-08-14 17:49:02 -0300

    add graylog configuration

--- config/logging.php
+++ config/logging.php
@@ -35,6 +35,20 @@ return [
     */
 
     'channels' => [
+        'graylog' => [
+            'driver' => 'monolog',
+            'level' => 'debug',
+            'handler' => \Monolog\Handler\GelfHandler::class,
+            'handler_with' => [
+                'publisher' => (new \Gelf\Publisher(new \Gelf\Transport\UdpTransport(
+                    config('graylog.host'),
+                    config('graylog.port'),
+                    \Gelf\Transport\UdpTransport::CHUNK_SIZE_LAN
+                ))),
+            ],
+            'formatter' => \Monolog\Formatter\GelfMessageFormatter::class,
+        ],
+
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
