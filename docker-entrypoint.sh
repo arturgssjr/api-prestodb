708f4127189caafee08e5f33c27879fe33bd7e4e
Artur Júnior <artur.junior@soluti.com.br>
2020-10-06 17:39:48 -0300

    comandos de cache ao inicializar aplicaçao

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,11 @@ chown -R www-data:www-data /var/www/html
 cd /var/www/html
 mv .env.example .env
 
-# php artisan config:clear
-# php artisan config:cache
-# php artisan route:cache
+php artisan key:generate --ansi
+php artisan optimize:clear
+php artisan package:discover
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 exec "$@"
