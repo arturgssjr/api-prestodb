f10893486c4b4675677d71ba5a52ef18c4880d08
Artur Júnior <artur.junior@soluti.com.br>
2020-10-06 17:39:00 -0300

    configurando rota para gerar cache

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,13 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@welcome')->name('welcome');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
 
 Route::namespace('OAuth')->prefix('oauth')->name('oauth.')->middleware('auth')->group(function () {
     Route::resource('/client-credentials', 'ClientCredentialsController');
