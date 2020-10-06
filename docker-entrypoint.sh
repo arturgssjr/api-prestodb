5c542bdb6d8a80dccf3462a434cf1d040d439a16
Artur Júnior <artur.junior@soluti.com.br>
2020-10-06 18:17:20 -0300

    add informaçao de inicializaçao da aplicaçao

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,20 @@ cp -R /var/www/tmp/. /var/www/html/
 chown -R www-data:www-data /var/www/html
 
 cd /var/www/html
+echo -n "### Renomeando arquivo .env"
 mv .env.example .env
 
+echo -n "### Gerando chave da aplicação"
 php artisan key:generate --ansi
+echo -n "### Limpando caches da aplicação"
 php artisan optimize:clear
+echo -n "### Gerando cache das dependências da aplicação"
 php artisan package:discover
+echo -n "### Gerando cache de configuração da aplicação"
 php artisan config:cache
+echo -n "### Gerando cache das rotas da aplicação"
 php artisan route:cache
+echo -n "### Gerando cache das views da aplicação"
 php artisan view:cache
 
 exec "$@"
