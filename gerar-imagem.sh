68a66ab0d0cf77b3ace41ad9bcda40758ea868bf
Artur Júnior <artur.junior@soluti.com.br>
2020-10-20 15:43:51 -0300

    add gerar imagem nginx

--- gerar-imagem.sh
+++ gerar-imagem.sh
@@ -2,7 +2,7 @@
 
 if [ -z $1 ]; then
     echo -e "[ERROR] Falha ao gerar imagem!"
-    echo -e "[INFO]  Ex.: ./gerar-imagem.sh php"
+    echo -e "[INFO]  Ex.: ./gerar-imagem.sh php|nginx"
     exit
 fi
 
@@ -21,6 +21,13 @@ if [ $1 == 'php' ]; then
     echo -e "[INFO] Enviando imagem para o Registry..."
     docker push arturgssjr/api-consulta-bigdata:latest
     docker push "arturgssjr/api-consulta-bigdata:${API_VERSION}"
-else
-    echo -e "[ERROR] Imagem $1 não existe!"
+fi
+
+if [ $1 == 'nginx' ]; then
+    echo -e "[INFO] Gerando imagem do NGINX..."
+    docker build -f docker/nginx/Dockerfile -t arturgssjr/api-consulta-bigdata:nginx .
+    echo -e "[INFO] Gerando TAG da imagem..."
+    docker image tag arturgssjr/api-consulta-bigdata:nginx arturgssjr/api-consulta-bigdata:nginx
+    echo -e "[INFO] Enviando imagem para o Registry..."
+    docker push arturgssjr/api-consulta-bigdata:nginx
 fi
