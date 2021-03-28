7c16fdf94abd344cefe50a9ea2b50a4462ed1ee7
Artur Júnior <arturgssjr@gmail.com>
2021-03-28 16:17:07 -0300

    upgrade to laravel 8

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -7,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
